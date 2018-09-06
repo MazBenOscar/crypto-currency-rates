09e02ea508a3337653be0e2d9f747de23e746929
Maz Ben Oscar <maz@benoscar.co>
2018-09-06 07:57:05 +0200

    Connected interface to API

--- js/cryptoAPI.js
+++ js/cryptoAPI.js
@@ -1,1 +1,14 @@
-class CryptoAPI {}
+class CryptoAPI {
+  // Get all cryptocurrencies
+  async getCryptoCurrenciesList() {
+    const url = await fetch("https://api.coinmarketcap.com/v1/ticker/");
+
+    //Return this is as json
+    const cryptoCurrencies = await url.json();
+
+    //Return the object
+    return {
+      cryptoCurrencies
+    };
+  }
+}
--- js/ui.js
+++ js/ui.js
@@ -1,1 +1,14 @@
-class UI {}
+class UI {
+  constructor() {
+    this.init();
+  }
+  init() {
+    this.printCryptoCurrencies();
+  }
+  // Prints the <option> for the form
+  printCryptoCurrencies() {
+    cryptoAPI.getCryptoCurrenciesList().then(data => {
+      const cryptoCurrencies = data.cryptoCurrencies;
+    });
+  }
+}
