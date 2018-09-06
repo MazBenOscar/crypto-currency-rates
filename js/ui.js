8435c27321b90fc59da1ddcd1a8e7fa2a0cc1754
Maz Ben Oscar <maz@benoscar.co>
2018-09-06 08:02:43 +0200

    Added Cryptocurrency dropdown options via API

--- js/ui.js
+++ js/ui.js
@@ -9,6 +9,17 @@ class UI {
   printCryptoCurrencies() {
     cryptoAPI.getCryptoCurrenciesList().then(data => {
       const cryptoCurrencies = data.cryptoCurrencies;
+
+      //   Build the <select> from the REST API
+      const select = document.getElementById("cryptocurrency");
+
+      cryptoCurrencies.forEach(currency => {
+        //add the option
+        const option = document.createElement("option");
+        option.value = currency.id;
+        option.appendChild(document.createTextNode(currency.name));
+        select.appendChild(option);
+      });
     });
   }
 }
