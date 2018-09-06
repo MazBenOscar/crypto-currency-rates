38ca16ba5519349c0aa5a52996f70ff674adb6fe
Maz Ben Oscar <maz@benoscar.co>
2018-09-06 08:16:40 +0200

    Added EventListener for user options

--- js/app.js
+++ js/app.js
@@ -2,3 +2,18 @@
 
 const cryptoAPI = new CryptoAPI();
 const ui = new UI();
+
+// Create the variables
+
+const form = document.getElementById("form");
+
+//add Event listener
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  // read currency
+  const currencySelect = document.getElementById("currency").value;
+  //read cryptocurrency
+  const cryptoCurrencySelect = document.getElementById("cryptocurrency").value;
+
+  console.log(currencySelect + ":" + cryptoCurrencySelect);
+});
